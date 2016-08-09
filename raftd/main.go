b93b38263a771da439e438f7378a778bcf8eb347
Injun Song <ijsong@gmail.com>
2016-08-10 00:16:41 +0900

    play raft!

---
+++ raftd/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/xsdb/playground/raftd/partition"
+)
+
+func main() {
+	partition := partition.NewPartition()
+}
---
+++ raftd/partition/partition.go
@@ -0,0 +1,94 @@
+package partition
+
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+	"time"
+
+	"github.com/hashicorp/raft"
+	"github.com/hashicorp/raft-boltdb"
+)
+
+const (
+	bindAddr = "0.0.0.0"
+	maxPool  = 128
+	timeout  = time.Second * 3
+)
+
+type LogCommitResult struct {
+	result int
+}
+
+type PartitionFSM struct {
+}
+
+type PartitionPeerStore struct {
+}
+
+type Partition struct {
+	raft *raft.Raft
+}
+
+func NewPartition() (*Partition, error) {
+	advertise, err := net.ResolveTCPAddr("tcp", bindAddr)
+	if err != nil {
+		return nil, err
+	}
+
+	logger := log.New(os.Stdout, "[transport]", log.LstdFlags)
+	trans, err := raft.NewTCPTransportWithLogger(bindAddr, advertise, maxPool, timeout, logger)
+	if err != nil {
+		return nil, err
+	}
+
+	conf := raft.DefaultConfig()
+
+	fsm := &PartitionFSM{}
+
+	logs, err := raftboltdb.NewBoltStore("/tmp/boltdb")
+	if err != nil {
+		return nil, err
+	}
+
+	logger = log.New(os.Stdout, "[snapshot]", log.LstdFlags)
+	snaps, err := raft.NewFileSnapshotStoreWithLogger("/tmp", 1, logger)
+	if err != nil {
+		return nil, err
+	}
+
+	peerStore := &PartitionPeerStore{}
+
+	raft, err := raft.NewRaft(conf, fsm, logs, logs, snaps, peerStore, trans)
+	if err != nil {
+		return nil, err
+	}
+
+	partition := &Partition{raft: raft}
+
+	return partition, nil
+}
+
+func (fsm *PartitionFSM) Apply(l *raft.Log) interface{} {
+	log.Println("[Apply to FSM] Index=", l.Index)
+	log.Println("[Apply to FSM] Term=", l.Term)
+	log.Println("[Apply to FSM] Type=", l.Type)
+	return &LogCommitResult{}
+}
+
+func (fsm *PartitionFSM) Snapshot() (raft.FSMSnapshot, error) {
+	return nil, nil
+}
+
+func (fsm *PartitionFSM) Restore(reader io.ReadCloser) error {
+	return nil
+}
+
+func (ps *PartitionPeerStore) Peers() ([]string, error) {
+	return []string{}, nil
+}
+
+func (ps *PartitionPeerStore) SetPeers([]string) error {
+	return nil
+}
