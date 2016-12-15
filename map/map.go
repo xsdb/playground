2a1ce92a3a10773a97581606e7203d5e1880de34
Injun Song <ijsong@gmail.com>
2016-12-15 23:29:17 +0900

    add

--- map/map.go
+++ map/map.go
@@ -40,6 +40,14 @@ func write(m map[string]*Value, key string) {
 	}
 }
 
+func update(m map[string]*Value, key string) {
+	defer wg.Done()
+	for i := 0; i < nrLoop; i++ {
+		v := fmt.Sprintf("value-%v", i)
+		m[key].value = v
+	}
+}
+
 func readSameKeys(m map[string]*Value) {
 	go read(m, "key-1")
 	go read(m, "key-1")
@@ -55,19 +63,38 @@ func writeSameKeys(m map[string]*Value) {
 	go write(m, "key-1")
 }
 
+func updateSameKeys(m map[string]*Value) {
+	go update(m, "key-1")
+	go update(m, "key-1")
+}
+
 func writeDiffKeys(m map[string]*Value) {
 	go write(m, "key-1")
 	go write(m, "key-2")
+}
+
+func updateDiffKeys(m map[string]*Value) {
+	go update(m, "key-1")
+	go update(m, "key-2")
 }
 
 func readWriteSameKeys(m map[string]*Value) {
 	go read(m, "key-1")
 	go write(m, "key-1")
 }
+func readUpdateSameKeys(m map[string]*Value) {
+	go read(m, "key-1")
+	go update(m, "key-1")
+}
 
 func readWriteDiffKeys(m map[string]*Value) {
 	go read(m, "key-1")
 	go write(m, "key-2")
+}
+
+func readUpdateDiffKeys(m map[string]*Value) {
+	go read(m, "key-1")
+	go update(m, "key-2")
 }
 
 func main() {
@@ -80,6 +107,11 @@ func main() {
 	// writeDiffKeys(m) // => fatal error: concurrent map writes
 	// readWriteSameKeys(m) // => fatal error: concurrent map read and map write
 	// readWriteDiffKeys(m) // => fatal error: concurrent map read and map write
+
+	// updateSameKeys(m) // => OK
+	// updateDiffKeys(m) // => OK
+	// readUpdateSameKeys(m) // => OK
+	// readUpdateDiffKeys(m) // => OK
 
 	wg.Wait()
 	log.Println("Finished")
